0dd4c16b7d51b31a91bed3e15035edf2546b047d
0060_Kunyaruch Panpin <kkunyaruch@gmail.com>
2026-08-24 16:17:08 +0700

    Update script.js

--- GAME2/script.js
+++ GAME2/script.js
@@ -37,8 +37,8 @@ let audioPlayer = new Audio();
 // audioPlayer.crossOrigin = "anonymous";
 
 let songList = [
-    { name: "🎵 ถิ่นชงโค", url: "./mp3/thinchongkho.mp3", bpm: 120 },
-    { name: "🎵 Boy Don't Cry", url: "./mp3/Boys_Dont_Cry.mp3", bpm: 120 }
+    { name: "🎵 ถิ่นชงโค", url: "./mp3/thinchongkho.mp3"},
+    { name: "🎵 Boy Don't Cry", url: "./mp3/Boys_Dont_Cry.mp3" }
 ];
 let currentSongIndex = 0;
 let nextSpawnTime = 0;
